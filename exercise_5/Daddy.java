23bd9ac8a4fdfa68b6088f9416e027e3d940bb0a
Tehrence Llenarez <121592180+teejx@users.noreply.github.com>
2023-02-10 10:07:54 +0800

    Add files via upload

---
+++ exercise_5/Daddy.java
@@ -0,0 +1,8 @@
+public class Daddy {
+    
+    protected static String surname = "Llenarez";
+    
+    public void showLove() {
+        System.out.println("I love my " + surname + " family.");
+    }
+}
